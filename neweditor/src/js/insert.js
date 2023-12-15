a573441905fcad7ce391b2ee6e411ce952431646
BUTERY Lucas <lucas.butery@imt-atlantique.net>
2023-12-15 10:13:55 +0100

    complete change of neweditor to frontend

--- neweditor/src/js/insert.js
+++
@@ -1,7 +0,0 @@
-
-
-// function insertquestion() {
-//     let insertQuestion ='<li id="textArea" > <label for="txtArea" class="labelQuestionTxt">Commentaire et commandes</label><textarea name="txt" cols="10" rows="1"></textarea></li>';
- 
-//     document.getElementById("etapeInitiale").insertAdjacentHTML('afterend', insertQuestion);
-// }
